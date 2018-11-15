9ed54a694886928f89adfb5c60fc7fd8f4b8afa4
Julian Napolitan <julian.napolitan@gmail.com>
2018-11-15 15:01:26 -0800

    Add bands

--- music_app/config/routes.rb
+++ music_app/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
+  resources :bands
 
   root to: 'users#new'
 end

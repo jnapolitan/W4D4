fba84f3a2a7889b1214d212b2894dd67a216e476
Julian Napolitan <julian.napolitan@gmail.com>
2018-11-15 13:55:10 -0800

    Update button styles and fix password length bug

--- music_app/config/routes.rb
+++ music_app/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
+
+  root to: 'users#new'
 end
